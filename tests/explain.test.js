bbd28e538fe67adcab04a00cfba5191ec61df989
Alex <zuedev@gmail.com>
2023-03-04 02:43:09 +0000

    add explain function tests

---
+++ tests/explain.test.js
@@ -0,0 +1,20 @@
+import Zorp from "../main.js";
+
+const zorp = new Zorp({
+  openai: {
+    apiKey: process.env.OPENAI_API_KEY,
+  },
+});
+
+test("the trade federation's blockade of Naboo", async () => {
+  const prompt = "The trade federation's blockade of Naboo.";
+  const response = await zorp.explain(prompt);
+
+  console.log({
+    prompt,
+    response,
+  });
+
+  expect(response).toBeTruthy();
+  expect(response.length).toBeGreaterThan(250);
+});
