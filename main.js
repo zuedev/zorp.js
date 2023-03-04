0431a400d353d91ceaedc7a7ce340efc452614b0
Alex <zuedev@gmail.com>
2023-03-04 02:29:14 +0000

    chat and answer are two different things

--- main.js
+++ main.js
@@ -21,23 +21,55 @@ export default class zorp {
       else throw new Error("OpenAI API key not provided");
   }
 
-  async chat(prompt, previousMessages = [], startup = null) {
+  async chat(prompt, previousMessages = [], personality = null) {
     if (!this.openai)
       throw new Error("This command requires an OpenAI API key");
 
     if (!prompt) throw new Error("Prompt not provided");
 
-    let startupDefault = "";
-    startupDefault += `You are a chatbot named ${this.configuration.zorp.name}.\n`;
-    startupDefault += `You were created by a human named Alex, a UK-based software developer who goes by "zuedev" on GitHub.\n`;
-    startupDefault += `Your messages should be short and to the point.\n`;
+    if (!personality) {
+      personality = "";
+      personality += `You are a chatbot named ${this.configuration.zorp.name}.\n`;
+      personality += `You were created by a human named Alex, a UK-based software developer who goes by "zuedev" on GitHub.\n`;
+      personality += `Your messages should be warm and friendly.\n`;
+    }
 
     const response = await this.openai.createChatCompletion({
       model: "gpt-3.5-turbo",
       messages: [
-        { role: "system", content: startup || startupDefault },
+        { role: "system", content: personality },
         ...previousMessages,
         { role: "user", content: prompt },
+      ],
+    });
+
+    return response.data.choices[0].message.content.trim();
+  }
+
+  async answer(question, previousMessages = [], personality = null) {
+    if (!this.openai)
+      throw new Error("This command requires an OpenAI API key");
+
+    if (!question) throw new Error("Question not provided");
+
+    if (!question.endsWith("?"))
+      throw new Error("Question must end with a question mark");
+
+    if (!personality) {
+      personality = "";
+      personality += `You are a chatbot named ${this.configuration.zorp.name}.\n`;
+      personality += `You were created by a human named Alex, a UK-based software developer who goes by "zuedev" on GitHub.\n`;
+      personality += `Your messages should be short and to the point.\n`;
+      personality += `Try to answer questions in one sentence, taking care to avoid being too vague or explaining too much.\n`;
+      personality += `At the very least, try to answer questions with a single "yes" or "no" answer.\n`;
+    }
+
+    const response = await this.openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: personality },
+        ...previousMessages,
+        { role: "user", content: question },
       ],
     });
 
---
+++ tests/answer.test.js
@@ -0,0 +1,126 @@
+import Zorp from "../main.js";
+
+const zorp = new Zorp({
+  openai: {
+    apiKey: process.env.OPENAI_API_KEY,
+  },
+});
+
+test("name check", async () => {
+  const prompt = "What is your name?";
+  const response = await zorp.answer(prompt);
+
+  console.log({
+    prompt,
+    response,
+  });
+
+  expect(response).toBeTruthy();
+  expect(response).toContain("Zorp");
+});
+
+test("creator check", async () => {
+  const name_prompt = "What is your creator's name?";
+  const name_response = await zorp.answer(name_prompt);
+
+  console.log({
+    name_prompt,
+    name_response,
+  });
+
+  expect(name_response).toBeTruthy();
+  expect(name_response).toContain("Alex");
+
+  const country_prompt = "What country is your creator from?";
+  const country_response = await zorp.answer(country_prompt);
+
+  console.log({
+    country_prompt,
+    country_response,
+  });
+
+  expect(country_response).toBeTruthy();
+  expect(country_response).toMatch(/(United Kingdom|UK)/);
+
+  const github_prompt = "What is your creator's GitHub username?";
+  const github_response = await zorp.answer(github_prompt);
+
+  console.log({
+    github_prompt,
+    github_response,
+  });
+
+  expect(github_response).toBeTruthy();
+  expect(github_response).toContain("zuedev");
+
+  const human_prompt = "Is your creator a human?";
+  const human_response = await zorp.answer(human_prompt);
+
+  console.log({
+    human_prompt,
+    human_response,
+  });
+
+  expect(human_response).toBeTruthy();
+  expect(human_response).toContain("Yes");
+});
+
+test("random string check", async () => {
+  const randomString = Math.random().toString(36).substring(7);
+  const notRandomString = "Hello, world!";
+
+  const yes_prompt = `Does "${randomString}" look like a random string?`;
+  const yes_response = await zorp.answer(yes_prompt);
+
+  console.log({
+    yes_prompt,
+    yes_response,
+  });
+
+  expect(yes_response).toBeTruthy();
+  expect(yes_response).toContain("Yes");
+
+  const no_prompt = `Does "${notRandomString}" look like a random string?`;
+  const no_response = await zorp.answer(no_prompt);
+
+  console.log({
+    no_prompt,
+    no_response,
+  });
+
+  expect(no_response).toBeTruthy();
+  expect(no_response).toContain("No");
+});
+
+test("random number sequence check", async () => {
+  const randomNumbers = Array.from({ length: 10 }, () =>
+    Math.floor(Math.random() * 10000)
+  );
+  const notRandomNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const yes_prompt = `Does "${randomNumbers.join(
+    ", "
+  )}" look like a random number sequence?`;
+  const yes_response = await zorp.answer(yes_prompt);
+
+  console.log({
+    yes_prompt,
+    yes_response,
+  });
+
+  expect(yes_response).toBeTruthy();
+  expect(yes_response).toContain("Yes");
+
+  const no_prompt = `Does "${notRandomNumbers.join(
+    ", "
+  )}" look like a random number sequence?`;
+  const no_response = await zorp.answer(no_prompt);
+
+  console.log({
+    no_prompt,
+    no_response,
+  });
+
+  expect(no_response).toBeTruthy();
+  expect(no_response).toContain("No");
+});
